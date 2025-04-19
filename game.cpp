6d7a48793546eb431b6ed069f1e6cf7f0530da17
Jesulayomi <olawuyijesulayomi@gmail.com>
2025-04-19 00:06:08 -0600

    yes

--- game.cpp
+++ game.cpp
@@ -11,18 +11,18 @@ int main() {
 
     cout << "Welcome to the number gussing game!" << endl;
     cout << "Guess any number between 1 and 100." << endl;
-    
-    cin >> guess;
-    tries ++;
-
-    if (guess > num) {
-        cout << "Too high, try again!" << endl;
-    } 
-        else if (guess < num) {
+    do {
+        cout << "Guess any number between 1 and 100." << endl;
+        cin >> guess;
+        tries ++;
+        if (guess > num) {
+          cout << "Too high, try again!" << endl;
+        } else if (guess < num) {
             cout << "Too low, try again!" << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << tries << " tries!" << endl;
         }
-            else {
-                cout << "Congratulations! You guessed the number in " << tries << " tries!" << endl;
-            }
-     return 0;
+    } while (guess != num);
+    
+    return 0;
 }
