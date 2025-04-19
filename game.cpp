babb2475f06e59723636eca244fbc8d240670f5c
Jesulayomi <olawuyijesulayomi@gmail.com>
2025-04-18 23:54:00 -0600

    game build

---
+++ game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int num = rand() % 100 + 1;
+    int guess;
+    int tries = 0;
+
+    cout << "Welcome to the number gussing game!" << endl;
+    cout << "Guess any number between 1 and 100." << endl;
+    
+    cin >> guess;
+    tries ++;
+
+    if (guess > num) {
+        cout << "Too high, try again!" << endl;
+    } 
+        else if (guess < num) {
+            cout << "Too low, try again!" << endl;
+        }
+            else {
+                cout << "Congratulations! You guessed the number in " << tries << " tries!" << endl;
+            }
+     return 0;
+}
